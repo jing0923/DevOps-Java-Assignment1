7cfaa199407d8e19c2fb2266a4d1f39d483a4710
jing0923 <jinghu0923@hotmail.com>
2019-10-03 12:57:48 -0700

    add new requirement

--- BankAccount.java
+++ BankAccount.java
@@ -1,15 +1,35 @@
 package com.flexon.corejava;
 
-public class BankAccount {
+import java.awt.*;
+import java.util.*;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+class BankAccount {
     private long accountNumber;
+    private String accountType;
     private double balance;
     private String customerFirstName;
     private String customerLastName;
     private String email;
     private int phoneNumber;
 
-    public BankAccount(long accountNumber, double balance, String customerFirstName, String customerLastName, String email, int phoneNumber){
+    private int depositNo = 0;
+    private double depositTotal = 0;
+    private double withdrawTotal = 0;
+
+
+    DateFormat timestampFormat = new SimpleDateFormat("HH:mm:ss MM/dd/yyyy");
+    Date timeStamp= new Date();
+
+    List<String> History = new ArrayList<>();
+
+    public BankAccount(long accountNumber, String accountType, double balance, String customerFirstName, String customerLastName, String email, int phoneNumber){
         this.accountNumber = accountNumber;
+        this.accountType = accountType;
         this.balance = balance;
         this.customerFirstName = customerFirstName;
         this.customerLastName = customerLastName;
@@ -17,25 +37,58 @@ public class BankAccount {
         this.phoneNumber = phoneNumber;
     }
 
-    public double getBalance() {
-        return balance;
+    public void showBalance() {
+        System.out.println("Name: " + customerFirstName + " " + customerLastName + "\n" + "AccountType: " + accountType + "\n" + "Balance: $ " + balance);
+        System.out.println(timestampFormat.format(timeStamp) + "\n");
+    }
+
+
+    public int getDepositNo() {
+        return depositNo;
+    }
+
+    public double getDepositTotal() {
+        return depositTotal;
     }
 
     public void depositFunds(double deposit) {
-        this.balance += deposit;
-        System.out.println("Deposit Successfully\n");
+            if(deposit>500) System.out.println("Sorry, the limitation for each transaction is $500.\n");
+            else if(deposit<50) System.out.println("Sorry, deposit less than $50 is not allowed.\n");
+            else {
+                this.balance += deposit;
+                depositTotal += deposit;
+                depositNo++;
+                String deposittransaction = "\nDoposit " + deposit + " at " + timeStamp + ", balance: $" + balance +". \n";
+                History.add(deposittransaction);
+                System.out.println("Deposit Successfully, now you balance is: $" + balance + "\n");
+            }
     }
 
     public void withdrawFunds(double withdraw) {
-        if(balance < withdraw) System.out.println(customerFirstName + " " + customerLastName + "\n sorry, you have insufficient balance: " + balance );
-        else {
-            this.balance -= withdraw;
-            System.out.println("Withdraw Successfully\n");
+        if(withdrawTotal<=1000){
+            if(balance < withdraw) System.out.println(customerFirstName + " " + customerLastName + "\n sorry, you have insufficient balance: $" + balance );
+            else {
+                this.balance -= withdraw;
+                withdrawTotal += withdraw;
+                double remain = 1000 - withdrawTotal;
+                String withdrawtransaction = "\nWithdraw " + withdraw + " at " + timeStamp + ", balance: $" + balance;
+                History.add(withdrawtransaction);
+                System.out.println("Withdraw Successfully, now your balance is: $" + balance + "\nYou can still withdraw $" + remain +"\n");
+            }
         }
+        else System.out.println("Sorry, the limitation for daily withdraw is $1000.\n");
     }
 
     public void printInfo(){
-        System.out.println("Customer Name: "+ customerFirstName + " " + customerLastName + "\nAccount Number: " + accountNumber + "\nBalance: " + balance + "\nEmail: " + email + "\nPhone Number: " + phoneNumber + "\n ");
+        System.out.println("Customer Name: "+ customerFirstName + " " + customerLastName + "\nAccount Number: " + accountNumber + "\nBalance: $" + balance + "\nEmail: " + email + "\nPhone Number: " + phoneNumber + "\n ");
+    }
+
+    public void transactionHistory(){
+        System.out.println("Here are all your transaction: \n" + History.toString()
+                .replace(",", "")  //remove the commas
+                .replace("[", "")  //remove the right bracket
+                .replace("]", "")  //remove the left bracket
+                .trim() + "\n");
     }
 
 }
--- FirstProgramm.java
+++ FirstProgramm.java
@@ -5,37 +5,40 @@ import java.util.Scanner;
 public class FirstProgramm {
     public static void main(String[] args) {
 
-        BankAccount myAccount = new BankAccount(123456789, 50.0, "Jing", "Hu", "jinghu0923@gmail.com", 111111111); //Hardcode for now
+        BankAccount myAccount = new BankAccount(123456789, "R", 50.00, "Jing", "Hu", "jinghu0923@gmail.com", 111111111); //Hardcode for now
 
-        while (true) {
-            System.out.println("Please select from the following: \n1. Account Information \n2. Account Balance \n3. Deposit Money \n4. Withdraw Money \n5. Exit");
+        Scanner input = new Scanner(System.in);
+        String flag = "";
 
-            Scanner input = new Scanner(System.in);
-            String flag = input.nextLine();
-            if (flag.equals("5")) break;
-
+        do {
+            System.out.println("Welcome to Bank! Please select from the following: \n1. Account Information \n2. Account Balance \n3. Deposit Money \n4. Withdraw Money \n5. Transaction History \n6.Exit");
+            flag =  input.nextLine();
             switch (flag) {
                 case "1":
                     myAccount.printInfo();
                     break;
                 case "2":
-                    System.out.println("Your balance is: " + myAccount.getBalance() +"\n");
+                     myAccount.showBalance();
                     break;
                 case "3":
-                    System.out.println("Please enter the amount money: "); //connect with API
-                    String deposit = input.nextLine();
-                    myAccount.depositFunds(Double.parseDouble(deposit));
+                    if(myAccount.getDepositNo()>=3) System.out.println("Sorry, 3 deposits are allowed daily.\n");
+                    else if(myAccount.getDepositTotal()>=1500) System.out.println("Sorry, $1500 deposits are allowed daily.\n");
+                    else {
+                        System.out.println("Please enter the amount money: "); //connect with API
+                        myAccount.depositFunds(Double.parseDouble(input.nextLine()));
+                    }
                     break;
                 case "4":
                     System.out.println("Please enter the amount of money you want to withdraw: ");
-                    String withdraw = input.nextLine();
-                    myAccount.withdrawFunds(Double.parseDouble(withdraw));
+                    myAccount.withdrawFunds(Double.parseDouble(input.nextLine()));
                     break;
+                case "5": myAccount.transactionHistory();break;
+                case "6": break;
                 default:
                     System.out.println("Wrong input!\n");
                     break;
             }
-        }
+        }while(!flag.equals("6"));
 
         System.out.println("Exit Successfully");
 
